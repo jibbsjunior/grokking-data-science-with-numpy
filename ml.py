48aafdd16ead8d6e95dcce62b600fda01200ed4f
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-11 18:12:40 +0100

    Quiz completed for Numpy Array

--- ml.py
+++ ml.py
@@ -41,13 +41,32 @@ import numpy as np
 # np.array([np.nan, 3, 2], dtype=np.int32) #uncommenting this line will break the code
 
 #Infinity method
-print(np.inf > 100000)
+# print(np.inf > 100000)
 
-arr = np.array([np.inf, 5])
-print(repr(arr))
+# arr = np.array([np.inf, 5])
+# print(repr(arr))
 
-arr = np.array([-np.inf, 2])
-print(repr(arr))
+# arr = np.array([-np.inf, 2])
+# print(repr(arr))
 
-np.array([np.inf, 3], dtype=np.float32)
+# np.array([np.inf, 3], dtype=np.float32)
 # np.array([np.inf, 2], dtype=np.int32) #uncommenting this line will break the code
+
+#Quiz
+#1. The first array we'll create comes straight from a list of integers and np.nan. The list contains np.nan as the first element, and the integers from 2 to 5, inclusive, as the next four elements. Set arr equal to np.array applied to the specified list.
+# arr = np.array([np.nan, 2, 3, 4, 5])
+# print(repr(arr))
+
+#2. We now want to copy the array so we can change the first element to 10. This way we don't modify the original array. Set arr2 equal to arr.copy(), then set the first element of arr2 equal to 10.
+# arr = np.array([np.nan, 2, 3, 4, 5])
+# arr2 = arr.copy()
+# arr2[0] = 10
+# print(repr(arr2))
+
+#3. The next two arrays will use floating point numbers. The first array will be upcast to floating point numbers, while we manually cast the second array using np.float32.
+# For manual casting, we use an array's inherent astype function, which takes in the new type as an argument and returns the casted array.
+# Set float_arr equal to np.array applied to a list with elements 1, 5.4, and 3, in that order.
+# Set float_arr2 equal to arr2.astype, with argument np.float32.
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+# float_arr2 = arr.astype(np.float32)
+# print(repr(float_arr2))
