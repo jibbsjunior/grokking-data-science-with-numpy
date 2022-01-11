60472812c73faf71f205cf7021b6314cbc00ec72
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-11 17:02:25 +0100

    create 2-D matrix with np

--- ml.py
+++ ml.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 
-arr1 = np.array([-2, 4, 5])
+# arr1 = np.array([-2, 4, 5])
+# print(arr1)
 
-print(arr1)
+#Create 2D matrix
+arr = np.array([[1, 2, 4], [3, 4, 5]], dtype=np.float32)
+print(repr(arr))
