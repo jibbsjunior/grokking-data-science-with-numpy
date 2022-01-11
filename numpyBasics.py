088675b57668cecd3a426aeb7e84698bcd095388
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-11 18:48:01 +0100

    linspace function

--- numpyBasics.py
+++ numpyBasics.py
@@ -1,5 +1,14 @@
 import numpy as np
+from numpy.core.arrayprint import array_repr
 
 # arange function
-arr = np.arange(5)
+# arr = np.arange(5)
+# print(repr(arr))
+
+#linspace function
+# arr = np.linspace(5, 11, num=5)
+# print(repr(arr))
+# arr = np.linspace(5, 11, num=4, endpoint=False)
+# print(repr(arr))
+arr = np.linspace(5, 12, num=6, dtype=np.float32)
 print(repr(arr))
