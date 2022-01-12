6109d11cbb1db241ddf45926efd1a5a5d546104b
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-12 16:34:07 +0100

    working with np.random.seed and np.random.shuffle func

--- numpyRandom.py
+++ numpyRandom.py
@@ -1,5 +1,25 @@
 import numpy as np
 
-print(np.random.randint(5))
-print(np.random.randint(5, high=6))
-print(np.random.randint(5, high=7, size=(2, 2)))
+# print(np.random.randint(5))
+# print(np.random.randint(5, high=6))
+# print(np.random.randint(5, high=7, size=(2, 2)))
+
+#Utility function
+# np.random.seed(1)
+# print(np.random.randint(10))
+# random_arr = np.random.randint(4, high=100, size=(2, 2))
+# print(random_arr)
+
+# #new seed
+# np.random.seed(4)
+# print(np.random.randint(23))
+# random_arr = np.random.randint(3, high=100, size=(2, 2))
+# print(random_arr)
+
+#np.random.shuffle function
+vec = np.array([1, 2, 3, 4, 5])
+np.random.shuffle(vec)
+print(vec)
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+np.random.shuffle(matrix)
+print(matrix)
