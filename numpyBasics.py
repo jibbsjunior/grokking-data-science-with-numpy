48da64c79df470976ea9a4600c1e1e3b715a027d
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-12 10:40:02 +0100

    working with transpose with the axes argument

--- numpyBasics.py
+++ numpyBasics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.core.arrayprint import array_repr
+from numpy.core.fromnumeric import reshape
 
 # arange function
 # arr = np.arange(5)
@@ -14,19 +15,31 @@ from numpy.core.arrayprint import array_repr
 # print(repr(arr))
 
 #reshape function
-arr = np.arange(8)
-reshaped_arr = np.reshape(arr, (2, 4))
-# print(repr(reshaped_arr.shape))
+# arr = np.arange(8)
+# reshaped_arr = np.reshape(arr, (2, 4))
+# # print(repr(reshaped_arr.shape))
 
-arr2 = np.arange(10)
-reshaped_arr2 = np.reshape(arr2, (-1, 2, 5))
-# print(repr(reshaped_arr2.shape))
+# arr2 = np.arange(10)
+# reshaped_arr2 = np.reshape(arr2, (-1, 2, 5))
+# # print(repr(reshaped_arr2.shape))
 
-#Flatten array
-arr3 = np.arange(12)
-reshaped_arr3 = np.reshape(arr3, (2, 6))
-flatten_arr = arr3.flatten()
-print(repr(arr3))
-print("Array shape: {}".format(reshaped_arr3.shape))
-print("Flatten array: {}".format(flatten_arr))
-print("Array after flatten: {}".format(flatten_arr.shape))
+# #Flatten array
+# arr3 = np.arange(12)
+# reshaped_arr3 = np.reshape(arr3, (2, 6))
+# flatten_arr = arr3.flatten()
+# print(repr(arr3))
+# print("Array shape: {}".format(reshaped_arr3.shape))
+# print("Flatten array: {}".format(flatten_arr))
+# print("Array after flatten: {}".format(flatten_arr.shape))
+
+#Transpose an array
+# arr = np.arange(8)
+# arr = np.reshape(arr, (2, 4))
+# transposed = np.transpose(arr)
+# print(repr(transposed.shape))
+
+#Transpose with the axes argument type
+arr = np.arange(24)
+arr = np.reshape(arr, (3, 4, 2))
+transposed = np.transpose(arr, axes=(2, 1, 0))
+print(repr(transposed))
