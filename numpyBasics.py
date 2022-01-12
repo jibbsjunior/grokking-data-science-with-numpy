7a693edd06776d1d3b157ff844de8ba5209f192b
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-12 09:01:27 +0100

    working with reshape function

--- numpyBasics.py
+++ numpyBasics.py
@@ -10,5 +10,10 @@ from numpy.core.arrayprint import array_repr
 # print(repr(arr))
 # arr = np.linspace(5, 11, num=4, endpoint=False)
 # print(repr(arr))
-arr = np.linspace(5, 12, num=6, dtype=np.float32)
-print(repr(arr))
+# arr = np.linspace(5, 12, num=6, dtype=np.float32)
+# print(repr(arr))
+
+#reshape function
+arr = np.arange(8)
+reshaped_arr = np.reshape(arr, (2, 4))
+print(repr(reshaped_arr.shape))
