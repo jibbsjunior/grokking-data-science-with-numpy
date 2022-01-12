0d28c0de388afdb51b402efd0b889ee0b701beab
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-12 18:03:58 +0100

    slicing through 1-D and 2-D numpy array

--- numpyIndexing.py
+++ numpyIndexing.py
@@ -1,9 +1,29 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5])
-print(arr[0])
-print(arr[4])
+#Array Accessing
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr[0])
+# print(arr[4])
 
-arr = np.array([[6, 3], [0, 2]])
-# Subarray
-print(repr(arr[0]))
+# arr = np.array([[6, 3], [0, 2]])
+# # Subarray
+# print(repr(arr[0]))
+
+#array slicing
+# arr = np.array([1, 2, 3, 4, 5])
+# print(repr(arr[:]))
+# print(repr(arr[1:]))
+# print(repr(arr[2:4]))
+# print(repr(arr[:-1]))
+# print(repr(arr[-2:]))
+
+#Slicing a 2-D numpy array
+arr = np.array([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]])
+print(repr(arr[:]))
+print(repr(arr[1:]))
+print(repr(arr[:, -1]))
+print(repr(arr[:, 1:]))
+print(repr(arr[0:1, 1:]))
+print(repr(arr[0, 1:]))
