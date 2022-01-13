89fde2f10eee3d97e31735e9e92a60b5ee304be4
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-13 15:02:49 +0100

    quiz completed on np.save and np.load

--- save_and_load_data.py
+++ save_and_load_data.py
@@ -7,3 +7,12 @@ np.save('arr', arr)
 #Load data
 load_file = np.load('arr.npy')
 print(repr(load_file))
+
+# #Quiz
+# 1. The coding exercise in this chapter will require you to complete the save_points function, which will save some randomly generated 2-D points in a file.
+# You'll generate 100 (x, y) points from a uniform distribution in the range [-2.5, 2.5), then save the points to save_file.
+def save_points(save_file):
+  # CODE HERE
+  points = np.random.uniform(low=-2.5, high=2.5, size=(100, 2))
+  np.save(save_file, points)
+  pass
